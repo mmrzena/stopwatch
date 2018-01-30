13abaae4ea4355df58a88353a03b51ec563ba954
Michal Mrzena <31210061+mmrzena@users.noreply.github.com>
2018-01-30 18:01:18 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,38 @@
-
-var running = 0;
+var running = false;
 var time = 0;
-var interval = setInterval(function() {
- if(running === 1){
-    time++;
-    document.getElementById('miliseconds').innerHTML = time;
+var interval;
+
+
+
+function start() {
+
+  if (!running) {
+  interval = setInterval(function() {
+      running = true;
+      time++;
+      var m = Math.floor(time / 100 / 60 % 60);
+      var s = Math.floor(time / 100 % 60);
+      var ms = time % 100;
+
+
+      document.getElementById('input').innerHTML = m + ":" + s + ":" + ms;
+
+  }, 10)
+  }
 }
-}, 1)
 
-
-
-document.getElementById('start').addEventListener("click", function(){
-  running = 1;
-  interval();
-})
-
-document.getElementById('stop').addEventListener("click", function(){
-
+function stop() {
   clearInterval(interval);
   running = 0;
+}
 
-})
+function clearr() {
+  time = 0;
 
+  document.getElementById('input').innerHTML = "";
+  // document.getElementById('miliseconds').innerHTML = "";
+  // document.getElementById('seconds').innerHTML = "";
+  // document.getElementById('minutes').innerHTML = "";
+  // document.getElementById('hours').innerHTML = "";
+
+}
